b5225c867eed9f965b5da306daecb74994c678fe
Chrizmo <chrizmo@gmail.com>
2019-02-13 21:01:23 +0100

    added watch to rebuild on save

--- gulpfile.js
+++ gulpfile.js
@@ -66,11 +66,11 @@ gulp.task('js', function(){
 });
 
 gulp.task('watch', function() {
-  gulp.watch('src/styles/**/*.scss', ['sass', 'jekyll-rebuild']);
+  gulp.watch('src/styles/**/*.scss', ['sass', 'host']);
   gulp.watch('src/js/**/*.js', ['js']);
   gulp.watch('src/fonts/**/*.{tff,woff,woff2}', ['fonts']);
   gulp.watch('src/img/**/*.{jpg,png,gif}', ['imagemin']);
-  gulp.watch(['*html', '_includes/*html', '_layouts/*.html'], ['jekyll-rebuild']);
+  gulp.watch(['*html', '_includes/*html', '_layouts/*.html'], ['host']);
 });
 
-gulp.task('default', ['js', 'sass', 'fonts', 'host']);
+gulp.task('default', ['js', 'sass', 'fonts', 'host','watch']);
